8724e3e1fbf20ef6374bccc1d47b86ca95ace36c
Moe El-Zein <melzein@spartaglobal.com>
2018-12-17 23:47:42 +0000

    Third commit

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
+// Nothing
 $(function(){
-
 
   var $character = $(".character");
   var $container = $(".container");
 
-  var guyMove = 0;
+  setUpEvents();
 
 // This is going to be so the character can jump
-  document.documentElement.addEventListener("keydown", function(event) {
-    if {
+  function setUpEvents() {
+    $(document.documentElement).keydown(function(event) {
+      if (event.keyCode == 32) {
+        jump();
+      }
+    });
+  }
 
-    }
+// This function handles the behaviour associated when jumping
+  function jump() {
+
+// Ensures no other keys can be pressed during the animation and keys can't be stacked
+    $(document.documentElement).off("keydown")
+
+// This calls the div character and animates it
     console.log("JUMP")
     $character.animate({
-      'top': "350px"
-// This goes up 350 from the top then back down to 400px
-    });
+      'top': "175px"
+    }, "fast");
+// This makes the character Div go down to 275px from the top then back up to 400px
     $character.animate({
-      'top': "400px"
-    });
-
-  });
+      'top': "350px"
+    }, "fast");
+// Sets the time interval between jumps or keystrokes
+    setTimeout(setUpEvents, 500);
+  }
 })
