edf5db83ed8651a1a1bd99cd27c9c5bcfe33749a
Moe El-Zein <melzein@spartaglobal.com>
2018-12-17 14:38:09 +0000

    Second commit

---
+++ main.js
@@ -0,0 +1,24 @@
+$(function(){
+
+
+  var $character = $(".character");
+  var $container = $(".container");
+
+  var guyMove = 0;
+
+// This is going to be so the character can jump
+  document.documentElement.addEventListener("keydown", function(event) {
+    if {
+
+    }
+    console.log("JUMP")
+    $character.animate({
+      'top': "350px"
+// This goes up 350 from the top then back down to 400px
+    });
+    $character.animate({
+      'top': "400px"
+    });
+
+  });
+})
